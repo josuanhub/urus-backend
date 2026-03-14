cfa47f7d6802eddce6bb484deb958c51f518fb71
josuanhub <urusgovx@gmail.com>
2026-03-13 21:12:24 -0400

    Update moltbook.routes.js

--- routes/moltbook.routes.js
+++ routes/moltbook.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const controller = require("../controllers/moltbook.controller");
+const controller = require("./controllers/moltbook.controller");
 
 const router = express.Router();
 
