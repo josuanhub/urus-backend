a19343c64548c44c45fcaf86929f8881cd7f9efb
josuanhub <urusgovx@gmail.com>
2026-04-14 22:32:02 -0400

    Update tenant.routes.js

--- routes/tenant.routes.js
+++ routes/tenant.routes.js
@@ -8,6 +8,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
+
 // Usa el pool global del server
 const getPool = () => global.__URUS_DB__;
 
