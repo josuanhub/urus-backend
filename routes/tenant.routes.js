f10cca37a8ae1439d18cbf8f657f76acc7c43629
josuanhub <urusgovx@gmail.com>
2026-04-15 14:40:47 -0400

    Update tenant.routes.js

--- routes/tenant.routes.js
+++ routes/tenant.routes.js
@@ -124,6 +124,7 @@ router.get('/leads/:id/messages', auth, async (req, res) => {
 router.post('/wa-config', auth, async (req, res) => {
   try {
     const pool = getPool();
+    console.log("USER DEBUG:", req.user);
     const userId = req.user.id;
 
     const {
