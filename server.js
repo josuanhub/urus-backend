38c565f79afffee3d999be6643ed5bb1438a352c
josuanhub <urusgovx@gmail.com>
2026-08-10 19:47:58 -0400

    Update server.js

--- server.js
+++ server.js
@@ -367,7 +367,7 @@ app.post(
 );
 // -------------------------------------------------------------------------------
 
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "25mb" }));
 app.use(express.urlencoded({ extended: false }));
 app.use("/v1/moltbook", moltbookRoutes);
 app.use("/v1/decision", decisionRoutes);
